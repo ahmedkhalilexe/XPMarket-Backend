b5a5558b87e22bf977d85974647df143cf2bf174
ahmed khalil amrani <amraniahmedkhalil@gmail.com>
2024-06-27 12:04:17 +0100

    feat: is admin middleware

--- private/Order/Order.private.js
+++ private/Order/Order.private.js
@@ -4,9 +4,9 @@ router.post(
   "/createOrder",
   require("../../controllers/orderController").createOrder
 );
-router.get("/getAllOrders", require("../../controllers/orderController").getAllOrders);
+router.get("/getAllOrders",require("../../middleware/authMiddleware").isAdmin, require("../../controllers/orderController").getAllOrders);
 router.get("/getAllOrdersByUser", require("../../controllers/orderController").getAllOrdersByUser);
 router.get("/getOrderById", require("../../controllers/orderController").getOrderById);
-router.put("/updateOrder", require("../../controllers/orderController").updateOrder);
-router.delete("/deleteOrder", require("../../controllers/orderController").deleteOrder);
+router.put("/updateOrder",require("../../middleware/authMiddleware").isAdmin, require("../../controllers/orderController").updateOrder);
+router.delete("/deleteOrder",require("../../middleware/authMiddleware").isAdmin, require("../../controllers/orderController").deleteOrder);
 module.exports = router;
